0dcdfe1aa126f0f092562401bb448d3a3f27ad32
HongTongHea <165672527+HongTongHea@users.noreply.github.com>
2024-10-23 21:42:13 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
-
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
@@ -20,7 +18,6 @@ Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth');
 Route::get('/users', [AuthController::class, 'viewUsers'])->name('users.index');
 Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
-
 
 Route::middleware('auth')->group(function () {
     Route::get('/dashboard', [UserController::class, 'dashboard'])->name('dashboard');
